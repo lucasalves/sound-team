c248a091e4c31f10d3343c4b0c79405fd433062b
Lucas Alves <lucas_alvessp@icloud.com>
2014-01-30 23:46:22 -0200

    add - constants  Views anda Components

---
+++ Resources/app/js/initializer/constants.js
@@ -0,0 +1,9 @@
+/**
+* define the constants for soudTeam
+*/
+
+//Views Path
+app.constant('VIEWS', 'app://app/views');
+
+//Component Search
+app.constant('COMPONENT_SEARCH', 'app://app/views/components/search/');
