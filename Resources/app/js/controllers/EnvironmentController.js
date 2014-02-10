64692d352e8f45a453bbc53c2942acb4079005c5
Lucas Alves <lucas_alvessp@icloud.com>
2014-02-10 21:52:07 -0200

    create - EnvironmentController
    responsável por controlar “Runner Test”

---
+++ Resources/app/js/controllers/EnvironmentController.js
@@ -0,0 +1,29 @@
+app.controller('EnvironmentController', function($scope){
+    var windowConfig = {
+            id: "test-runner",
+            title: "Test Runner",
+            url: "app://test/SpecRunner.html"
+    };
+
+    $scope.runTest = function(){
+        environment.setEnv('test');
+
+        $scope.windowRunnerTest = Ti.UI.createWindow(windowConfig);
+        $scope.windowRunnerTest.open();
+        $scope.windowRunnerTest.setTopMost(true);
+
+        this.addEventListener();
+    };
+
+    $scope.addEventListener = function(){
+        $scope.windowRunnerTest.addEventListener('closed', function(){
+            environment.setEnv( environment.getPrevEnv() );
+            
+            $scope.$apply();
+        });
+    };
+
+    $scope.isEnvironmentDevelopment = function(){
+        return environment.ENV === 'development';
+    };
+});
