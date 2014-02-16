a972ffb68f144c658a43da9f4c8e3805a3acbd85
Lucas Alves <lucas_alvessp@icloud.com>
2014-02-16 14:03:36 -0300

    fix - load file sql

--- Resources/app/js/initializer/schema.js
+++ Resources/app/js/initializer/schema.js
@@ -7,13 +7,23 @@ var Schema = {
             this._run('drop.sql');  
         },
 
-        _run: function(file){
-            var fileSql = Ti.Filesystem.getFile( Ti.Filesystem.getApplicationDataDirectory(), file)
-              , Stream = Ti.Filesystem.Filestream.getFileStream(fileSql);
+        _directory: 'app/schema/',
 
-            Stream.open(Ti.Filesystem.MODE_WRITE);
-            sql = Stream.read();            
-            DB.execute(sql);
-            Stream.close();
+        _run: function(file){
+            file = this._directory + file;
+            
+            var doc     = Ti.Filesystem.getFileStream(Ti.Filesystem.getResourcesDirectory(), file)
+              , command = []
+            ;
+
+            doc.open(Ti.Filesystem.MODE_READ);
+            sql = doc.read();
+            
+            command = sql.toString().split(/--/);
+            for (var i = 0; i < command.length; i++) {
+                DB.execute(command[i].replace(/\n/g, " "));
+            };
+             
+            doc.close();
         }
 };
