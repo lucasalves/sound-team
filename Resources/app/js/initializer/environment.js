5485bc36ebff956e245c49fe35fd41b6c5269b69
Lucas Alves <lucas_alvessp@icloud.com>
2014-02-16 14:20:47 -0300

    update - add method execOnChange

--- Resources/app/js/initializer/environment.js
+++ Resources/app/js/initializer/environment.js
@@ -9,11 +9,17 @@ var environment = {
             if(this.defaultsEnv.indexOf(env) > -1 && env !== this.ENV){
                 this._prevEnv = this.ENV;
                 this.ENV = env;
+
+                this.execOnChange();
             }
         },
         
         getPrevEnv: function(){
             return this._prevEnv;
+        },
+
+        execOnChange: function () {
+            Database.reconnect();
         }
 
 
