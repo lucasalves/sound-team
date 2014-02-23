b75df4a3fd2d3bbfb4c85e4283e10cc31f495ac8
Lucas Alves <lucas_alvessp@icloud.com>
2014-02-22 23:45:18 -0300

    fix - url path

--- Resources/app/js/initializer/constants.js
+++ Resources/app/js/initializer/constants.js
@@ -2,8 +2,12 @@
 * define the constants for soudTeam
 */
 
+
 //Views Path
-app.constant('VIEWS', 'app://app/views');
+app.constant('VIEWS', '/app/views');
 
 //Component Search
-app.constant('COMPONENT_SEARCH', 'app://app/views/components/search/');
+app.constant('COMPONENT_SEARCH', '/app/views/search/');
+
+//Component Search
+app.constant('COMPONENT_MOVIE', '/app/views/movie/');
