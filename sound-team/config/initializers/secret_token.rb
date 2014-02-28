582d67bc6fa7719e65533ec05b7d8df6df9ec67a
Lucas Alves <lucas_alvessp@icloud.com>
2014-02-27 22:59:18 -0300

    create rails project

---
+++ sound-team/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SoundTeam::Application.config.secret_key_base = '9f139bb11d8ec700337246749f9e292e62917fb6ec5ee40c148cdfa672c53df8af5a59e21f7b384012dfbdb8e5c13f7670dd3d393a75c51bfe6e61004968f365'
