5a234fb2ca2af690359788c3c981faa7b701099e
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-03 15:54:20 -0300

    add - dashboard controller and action index

--- sound-team/config/routes.rb
+++ sound-team/config/routes.rb
@@ -1,4 +1,5 @@
 SoundTeam::Application.routes.draw do
+  get 'dashboard' => 'dashboard#index', as: :dashboard
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
