9995216a247f12061fc22218e23071f5ba56eaf7
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-03 17:01:02 -0300

    update - name app angular

--- sound-team/app/assets/javascripts/initializer.js
+++ sound-team/app/assets/javascripts/initializer.js
@@ -1,1 +1,10 @@
-var app = angular.module('SoundTeam', []);
+var app = angular.module('soundTeam', []);
+    app.run(function($http, $rootScope){
+        var url = location.origin + '/init-data.json';
+        
+        $http.get(url)
+             .success(function(data){
+                $rootScope.$emit('init data', data);
+                $rootScope.$broadcast('init data', data);
+             });
+    });
