c6862a9e0ca4833d73dde9d7a34f244ea9849120
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-03 17:01:44 -0300

    update - routes add initialize json and root page

--- sound-team/config/routes.rb
+++ sound-team/config/routes.rb
@@ -1,10 +1,12 @@
 SoundTeam::Application.routes.draw do
   get 'dashboard' => 'dashboard#index', as: :dashboard
+
+  get 'init-data.json' => 'initialize#data'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'dashboard#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
