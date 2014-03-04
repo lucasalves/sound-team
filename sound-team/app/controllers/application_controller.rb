0f7d19183c1ffd946910620471d37ffad05bd6dc
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-04 20:24:48 -0300

    no load layout in ajax request

--- sound-team/app/controllers/application_controller.rb
+++ sound-team/app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_action :display_layout?
+
+  def display_layout?
+    if request.xhr?
+        render layout: false
+    end
+  end
 end
