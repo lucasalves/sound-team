57cc1ca4477395941d9fd11e2444bd4e4411f872
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-10 23:46:04 -0300

    add - test units Menu model

--- sound-team/test/models/menu_test.rb
+++ sound-team/test/models/menu_test.rb
@@ -4,4 +4,34 @@ class MenuTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should require name" do
+    menu = create(:name => nil)
+    assert menu.invalid?(:name), ":name should be required"
+  end
+
+  test "should contain more than 3 characters" do
+    menu = create(:name => 'Hi!')
+    assert menu.invalid?(:name), ":name should be greater than 3 characters" 
+  end
+
+  test "should respond to MenuItems" do      
+    menu = create
+    assert menu.respond_to?(:items), "should be respond to :items <- MenuItems"
+  end
+
+  test "should deny duplicate user" do
+      menu = create
+      assert menu.valid?
+
+      menu = create
+      assert menu.invalid?, "Menu should not be created"
+  end
+
+  private 
+    def create(options={})
+        Menu.create({
+            :name => 'Main Menu'
+        }.merge(options))
+    end
 end
