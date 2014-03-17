3153a02ed45189c16f9d16833426bd2c1f6c1326
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-17 20:53:00 -0300

    add - unit test MenuItem

--- sound-team/test/models/menu_item_test.rb
+++ sound-team/test/models/menu_item_test.rb
@@ -1,7 +1,56 @@
 require 'test_helper'
 
 class MenuItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should responde to componet" do
+    item = create()
+    assert item.respond_to?(:component), "Should be respond to reference Component"
+  end
+
+  test "Should require component" do
+    item = create(:component => nil)
+    assert item.errors[:component].any?, ":component should be required"
+
+    item = create(:component_id => 1000)
+    assert item.errors[:component].any?, "Component doesn't exist, so it should be required"
+  end
+
+  test "Should responde to menu" do
+    item = create()
+    assert item.respond_to?(:menu), "Should be respond to reference Menu"
+  end
+
+  test "Should require menu" do 
+    item = create(:menu => nil)
+    assert item.errors[:menu].any?, ":menu should be required"
+
+    item = create(:menu_id => 1000)    
+    assert item.errors[:menu].any?, "Menu doesn't exist, so it should be required"
+  end
+
+  test "Should require name" do
+    item = create(:name => nil)
+    assert item.errors[:name].any?, ":name should be required"
+  end
+
+  test "Should require icon" do
+    item = create(:icon => nil)
+    assert item.errors[:icon].any?, ":icon should be required"
+  end
+
+  test "Should require path" do
+    item = create(:path => nil)
+    assert item.errors[:path].any?, ":path should be required"
+  end
+
+  private
+    def create(options={})
+      MenuItem.create({
+        :name => "Dashboard",
+        :description => "Home application",
+        :icon => "dashboard",
+        :path => "/",
+        :menu => Menu.last,
+        :component => Component.last
+      }.merge(options))
+    end
 end
