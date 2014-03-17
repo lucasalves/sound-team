2d7c6c12d149c6c9e122ca50e0d12a981a5de00c
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-17 20:53:14 -0300

    add - validates MenuItem

--- sound-team/app/models/menu_item.rb
+++ sound-team/app/models/menu_item.rb
@@ -1,4 +1,10 @@
 class MenuItem < ActiveRecord::Base
   belongs_to :menu
   belongs_to :component
+
+  validates :component, presence: true
+  validates :menu, presence: true
+  validates :name, presence: true
+  validates :icon, presence: true
+  validates :path, presence: true
 end
