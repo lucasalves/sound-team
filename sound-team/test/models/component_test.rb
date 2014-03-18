53b600607dfd94198f91b0560ac2481d93e93417
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-17 22:11:38 -0300

    add - Component model validation name

--- sound-team/test/models/component_test.rb
+++ sound-team/test/models/component_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ComponentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should require name" do
+    component = create(name: nil)
+    assert component.errors[:name].any?, ":name should be required"
+  end
+
+  def create(options)
+    Component.create({
+      name: "Media Administrator",
+      description: "The average administrator allows you to manage all your video data, images and audio"
+    }.merge(options))
+  end
 end
