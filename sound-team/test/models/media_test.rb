7ce3a6f04b3c7059714caee3731a7121d95f2aaa
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-17 22:32:56 -0300

    add - test units validates model Media

--- sound-team/test/models/media_test.rb
+++ sound-team/test/models/media_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class MediaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should require name" do
+    media = create(:name => nil)
+    assert media.erros[:name].any?, ":name should be required"
+  end
+
+  test "should require format" do
+    media = create(:format => nil)
+    assert media.erros[:format].any?, ":format should be required"
+  end
+
+  test "should require size" do
+    media = create(:size => nil)
+    assert media.erros[:size].any?, ":size should be required"
+  end
+
+  test "should require path" do
+    media = create(:path => nil)
+    assert media.erros[:path].any?, ":path should be required"
+  end
+
+  test "should require type" do
+    media = create(:type => nil)
+    assert media.erros[:type].any?, ":type should be required"
+  end
+
+  def create(options)
+    Media.create({
+      :name => "Melhor Lugar do Mundo",
+      :description => "Musica melhor lugar do mundo",
+      :legend => "...",
+      :format => "mp3",
+      :size => 14677333,
+      :path => 'songs/2014/03/mlm',
+      :type => 'song'
+    }.merge(options))
+  end
 end
