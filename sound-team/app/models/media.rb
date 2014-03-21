f7eefb11fab8cfdbf5be463a814dbbff1b7a4fb6
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-20 22:10:49 -0300

    add - validate presence album

--- sound-team/app/models/media.rb
+++ sound-team/app/models/media.rb
@@ -3,5 +3,5 @@ class Media < ActiveRecord::Base
     belongs_to :component  
     has_and_belongs_to_many :categories
 
-    validates :name, :format, :size, :path, :kind, presence: true
+    validates :name, :format, :size, :path, :kind, :album, presence: true
 end
