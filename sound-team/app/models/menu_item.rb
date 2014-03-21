07f8e912b2dcfaae53676da7d65ba7d72e0c6170
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-20 21:45:07 -0300

    refactoring - validates MenuItem

--- sound-team/app/models/menu_item.rb
+++ sound-team/app/models/menu_item.rb
@@ -2,9 +2,5 @@ class MenuItem < ActiveRecord::Base
   belongs_to :menu
   belongs_to :component
 
-  validates :component, presence: true
-  validates :menu, presence: true
-  validates :name, presence: true
-  validates :icon, presence: true
-  validates :path, presence: true
+  validates :component, :menu, :name, :icon, :path, presence: true
 end
