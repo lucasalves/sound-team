55e1c395376ad79edc5c818e5ad9cae0769bf690
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-20 21:45:30 -0300

    update - add validates Media

--- sound-team/app/models/media.rb
+++ sound-team/app/models/media.rb
@@ -1,6 +1,7 @@
 class Media < ActiveRecord::Base
     belongs_to :album
-    belongs_to :component    
-
+    belongs_to :component  
     has_and_belongs_to_many :categories
+
+    validates :name, :format, :size, :path, :kind, presence: true
 end
