e21f660b682f54ffdc982dbc729c0a0be8df4851
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-20 22:08:54 -0300

    remove - test unnecessary

--- sound-team/test/models/album_test.rb
+++ sound-team/test/models/album_test.rb
@@ -15,27 +15,13 @@ class AlbumTest < ActiveSupport::TestCase
     album = create(:date => nil)
     assert album.errors[:date].any?, ":date should be required"
   end
-
-  test "should respond to media" do
-    album = create()
-    assert album.respond_to?(:media), "should be respond to reference Media"
-  end
-
-  test "should require media" do
-    album = create(:media => nil)
-    assert album.errors[:media].any?, ":media should be required"
-
-    album = create(:media_id => 1000)
-    assert album.errors[:media].any?, "media doesn't exist, so it should be required"
-  end
-
+  
   private
     def create(options={})
       Album.create({
         :name => "CD Jovem 2014",
         :description => "CD Jovem 2014 desc",
         :date => Date.parse("2014-03-20"),
-        :media => Media.last
       }.merge(options))
     end
 end
