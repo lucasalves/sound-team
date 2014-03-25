46aaa15f1080bd8f32287f265afb4811a4a01aeb
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-25 20:48:12 -0300

    add test name is required in Category

--- sound-team/test/models/category_test.rb
+++ sound-team/test/models/category_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should requite name" do
+    category = create(:name => nil)
+    assert category.errors[:name].any?, ":name should be required"
+  end
+
+  private
+    def create(options={})
+      Category.create({
+        :name => "Fundo Musical"
+      }.merge(options))
+    end
 end
