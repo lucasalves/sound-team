71c558bec7a85059ef138e38105e586daba5c2fd
Lucas Alves <lucas_alvessp@icloud.com>
2014-03-27 23:08:07 -0300

    add implementation method normalize

--- sound-team/app/models/media.rb
+++ sound-team/app/models/media.rb
@@ -1,7 +1,16 @@
 class Media < ActiveRecord::Base
     belongs_to :album
-    belongs_to :component  
+    belongs_to :component
     has_and_belongs_to_many :categories
 
     validates :name, :format, :size, :path, :kind, :album, presence: true
+
+    def self.kind_normalize(kind)
+      case kind
+      when 'songs'  then 'song'
+      when 'images' then 'image'
+      when 'movies' then 'movie'
+      else kind
+      end
+    end
 end
