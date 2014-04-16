891e3f7105630b339a11f1340b3d6dc919459f25
Lucas Alves <lucas_alvessp@icloud.com>
2014-04-16 20:49:43 -0300

    add method screenshot image_tag

--- sound-team/app/helpers/media_helper.rb
+++ sound-team/app/helpers/media_helper.rb
@@ -1,2 +1,9 @@
 module MediaHelper
+  def screenshot(media, options={})
+    path = '/assets/images/no-image.png'
+    if !media.information[:image].nil?
+        path = media.information[:image][:img_320x180]
+    end
+    image_tag(path, options)
+  end
 end
