b65d8bda5f3b8246376690b2d042ee2115983496
Lucas Alves <lucas_alvessp@icloud.com>
2014-04-23 00:04:10 -0300

    layout  media

--- sound-team/app/helpers/media_helper.rb
+++ sound-team/app/helpers/media_helper.rb
@@ -1,7 +1,7 @@
 module MediaHelper
   def screenshot(media, options={})
-    path = '/assets/images/no-image.png'
-    if !media.information[:image].nil?
+    path = '/assets/no-image.png'
+    if !media.information.nil? && !media.information[:image].nil?
         path = media.information[:image][:img_320x180]
     end
     image_tag(path, options)
